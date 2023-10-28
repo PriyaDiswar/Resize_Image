ac81273c8d52bc6a154163bd1635aee41b161d1d
Priya Chaudhary <priya22diswar@gmail.com>
2023-10-28 15:35:50 +0530

    adding labels and button properties

--- structure.py
+++ structure.py
@@ -5,13 +5,14 @@ from tkinter import *
 root = Tk()			 
 
 # Open window having dimension 100x100
-root.geometry('700x500') 
+root.geometry('1000x800') 
+
+label= Label(root, text = "Resize your image here!",font=("Arial",27)).place(x=320,y=10)
 
 # Create a Button
-btn = Button(root, text = 'Click me !', bd = '5') 
 
-# Set the position of button on the top of window. 
-btn.pack(side = 'top') 
+btn = Button(root, text = 'Select File', bd = '5',bg='Blue',font=("Arial",20))
+btn.place(x=420,y=100)
 
 root.mainloop()
 				 
